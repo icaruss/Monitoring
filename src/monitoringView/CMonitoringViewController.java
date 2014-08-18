f928285eda9f7ce9906f012ecdfe94848a95535f
icaruss <zorik.icarus@gmail.com>
2014-08-19 00:12:54 +0300

    #In the middle of updating tables

--- src/monitoringView/CMonitoringViewController.java
+++ src/monitoringView/CMonitoringViewController.java
@@ -5,23 +5,30 @@
 package monitoringView;
 
 import java.awt.Desktop;
-import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.Vector;
 
-import javax.imageio.ImageIO;
-import javax.swing.JSplitPane;
-import javax.swing.plaf.SplitPaneUI;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.formula.ptg.TblPtg;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.*;
 
-import mainView.Main;
 import viewLogic.CSharedInstance;
 import viewLogic.CViewConstants.DirectoryDirection;
 import viewLogic.CViewConstants.FileType;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -30,10 +37,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.SplitPane.Divider;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.DragEvent;
+
 
 // TODO: Auto-generated Javadoc
 /**
@@ -165,23 +173,32 @@ public class CMonitoringViewController implements Initializable {
 		
 
 		// opens all files associated with that current Run
-		btnShowInFile.setOnAction(new EventHandler<ActionEvent>() {
+		btnShowInFile.setOnAction(new EventHandler<ActionEvent>() 
+		{
 
 			@Override
-			public void handle(ActionEvent event) {
+			public void handle(ActionEvent event) 
+			{
 
 				Vector<String> vecOfDataFiles = CSharedInstance.getInstance()
 						.getCurrentDataFilesSet();
 
-				if (vecOfDataFiles != null && !vecOfDataFiles.isEmpty()) {
-					for (String fileName : vecOfDataFiles) {
+				if (vecOfDataFiles != null && !vecOfDataFiles.isEmpty())
+				{
+					for (String fileName : vecOfDataFiles) 
+					{
 						File f = new File(fileName);
-						if (f.exists() && !f.isDirectory()) {
-							try {
-								if (Desktop.isDesktopSupported()) {
+						if (f.exists() && !f.isDirectory()) 
+						{
+							try 
+							{
+								if (Desktop.isDesktopSupported())
+								{
 									Desktop.getDesktop().open(f);
 								}
-							} catch (IOException e) {
+							} 
+							catch (IOException e) 
+							{
 								e.printStackTrace();
 							}
 						}
@@ -358,7 +375,8 @@ public class CMonitoringViewController implements Initializable {
 
 	}
 
-	private void setExcelFileToViewByIndex() {
+	private void setExcelFileToViewByIndex() 
+	{
 		if (vectorOfExcelFiles.size() == 0)
 			return;
 
@@ -367,6 +385,8 @@ public class CMonitoringViewController implements Initializable {
 		} else if (currentIndexOfTables < 0) {
 			currentIndexOfTables += vectorOfExcelFiles.size();
 		}
+		
+		loadTableData(vectorOfExcelFiles.elementAt(currentIndexOfTables));
 
 	}
 
@@ -386,5 +406,82 @@ public class CMonitoringViewController implements Initializable {
 		
 		System.out.println(divider.getPosition());
 	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public void loadTableData(File file)
+	{
+		
+		Map <Integer,Vector<String>> map = new HashMap<Integer, Vector<String>>();
+		
+		
+		// retrieving all columns from current file
+		try
+		{
+			FileInputStream fileInput = new FileInputStream(file);
+
+			HSSFWorkbook workbook = new HSSFWorkbook(fileInput);
+			HSSFSheet sheet = workbook.getSheetAt(0);
+			
+			for (int i = sheet.getFirstRowNum() ; i <= sheet.getLastRowNum() ; ++i)
+			{
+				HSSFRow row_ = sheet.getRow(i);
+
+				for (int j = row_.getFirstCellNum() ; j < row_.getLastCellNum() ; ++j)
+				{
+					HSSFCell cell_ = row_.getCell(j);
+					
+					if (i == 0)
+					{
+						Vector<String> vec = new Vector<String>();
+	            		
+	            		vec.add(cell_.getStringCellValue());
+	            		
+	            		map.put(j, vec);
+					}
+					else
+	            	{
+	            		Vector<String> vec = map.get(j);
+	            		vec.add(cell_.getStringCellValue());
+	            	}
+					
+				}
+			}
+		}
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	
+		
+		// STILL IN PROGRESS
+		
+		/*// Updating table view with columns
+		for (Integer i : map.keySet())
+		{
+			Vector<String> vec = map.get(i);
+			
+			TableColumn tblColumn = new TableColumn();
+			tblColumn.
+			
+			//tbView.getColumns().add(new Table);
+		}*/
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
